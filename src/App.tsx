e31e9a5f5ec2be2037ce74c0b87eaac9d7d236cb
PhpDeveloper <kashish@castledigital.com.au>
2025-11-17 16:36:31 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import RealtimeVoiceChatMP3 from './RealtimeVoiceChatMP3'
 
 function App() {
- 
-
   return (
     <>
     <RealtimeVoiceChatMP3 />
-    
     </>
   )
 }
